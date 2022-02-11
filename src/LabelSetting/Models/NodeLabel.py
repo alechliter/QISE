f7e5eb5e03afd4be1c7f3beac53a31bf3dcd1106
alechliter <lechliter.2@osu.edu>
2022-02-11 16:00:57 -0500

    Began implementing the NodeLabel class

--- src/LabelSetting/Models/NodeLabel.py
+++ src/LabelSetting/Models/NodeLabel.py
@@ -11,11 +11,17 @@ class NodeLabel:
         + incoming_nodes (Sequence): a list of indicies of nodes with outgoing edges to this node.
     """
 
-    def __init__(self, index: int) -> None:
-        # TODO: finish
+    def __init__(self, index: int, incoming_nodes: Sequence = []) -> None:
+        """
+        Creates a new instance of a NodeLabel object.
+
+        Args:
+            index (int): the index identifier of the node
+            incoming_nodes (Sequence, optional): the list of nodes with outgoing edges going to the node. Defaults to [].
+        """
         self.node_index = index
-        self.labels: Mapping = {}
-        self.incoming_nodes = []
+        self.incoming_nodes = incoming_nodes
+        self.labels: Mapping = { node: [] for node in incoming_nodes }
 
     def add_label(self, weight: int, cost: int, index: int) -> None:
         """
@@ -26,8 +32,10 @@ class NodeLabel:
             cost (int): the total cost of the label
             index (int): the index of the node where the edge came from
         """
-        # TODO: implement
-        pass
+        if index not in self.incoming_nodes:
+            self.incoming_nodes.append(index)
+        
+        self.labels[index] = (weight, cost)
 
     def is_label_dominated(self, weight: int, cost: int) -> bool:
         """
@@ -36,23 +44,52 @@ class NodeLabel:
         of labels for this node. 
 
         Args:
-            weight (int): [description]
-            cost (int): [description]
+            weight (int): the weight of the label
+            cost (int): the cost of the label
 
         Returns:
-            bool: [description]
+            bool: true if the label is dominated by at least one label in this node
         """
-        # TODO: implement
-        pass
 
-    def get_efficient_labels(self) -> Sequence:
+        return NodeLabel._is_label_dominated_in_list(weight, cost, self.labels.value())
+
+    def get_efficient_labels(self) -> Mapping:
         """
         Returns a list of all the efficient labels for the node. An efficient label is a label
         that is not dominated by any other label for the node.
 
         Returns:
-            Sequence: [description]
+            Mapping: a dictionary of the nodes to their label, only returning efficient labels
         """
-        # TODO: implement
-        pass
+        efficient_labels = {}
+        
+        for node, label in self.labels.items():
+            other_labels = [ item for item in self.labels.values() if item != label ]
+            if not NodeLabel._is_label_dominated_in_list(label[0], label[1], other_labels):
+                efficient_labels[node] = label
+
+        return efficient_labels
+    
+    def _is_label_dominated_in_list(weight: int, cost: int, labels: Sequence) -> bool:
+        """
+        Checks if a label is dominated based on the weight and cost. A label is dominated if
+        its weight and cost are both less than the weight and cost of another node in the set
+        of labels for this node.
+
+        Args:
+            weight (int): the weight of the label
+            cost (int): the cost of the label
+            labels (Sequece): a list of labels
+
+        Returns:
+            bool: true if the label is dominated by at least one label in the list of labels
+        """
+        isDominated = False
+
+        for label in labels:
+            if (weight > label[0] and cost > label[1]) or label == (weight, cost):
+                isDominated = True
+                break
+
+        return isDominated
     
