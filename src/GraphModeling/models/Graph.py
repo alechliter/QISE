cd2e99031c8646489f99a6ca59155cdc15737c0a
connorbeheydt <18cbeheydt@gmail.com>
2022-02-12 14:12:36 -0500

    Added suppress_text_output parameter to print_graph for both Graph and WCGraph classes.

--- src/GraphModeling/models/Graph.py
+++ src/GraphModeling/models/Graph.py
@@ -33,23 +33,26 @@ class Graph:
         self._initialize_networkx_graph()
 
         
-    def print_graph(self, picture_name: str = "", edge_labels: Mapping | None = None) -> None:
+    def print_graph(self, picture_name: str = "", edge_labels: Mapping | None = None, suppress_text_output: bool = False) -> None:
         """
         Prints the graph using the networkx graph representation and pyplot.
 
         Args:
             picture_name (str, optional): The name to save the graph image as. Defaults to "".
             edge_labels (Mapping | None, optional): The dictionary of edges to labels. Defaults to None.
+            suppress_text_output: Supresses print statements and edge labels from being printed on the graph
         """
-        print("Nodes on Graph:")
-        print(f"|    {self.networkx_graph.nodes()}")
-        print("Edges of Graph:")
-        print(f"|    {self.networkx_graph.edges()}")
+        if(not suppress_text_output):
+            print("Nodes on Graph:")
+            print(f"|    {self.networkx_graph.nodes()}")
+            print("Edges of Graph:")
+            print(f"|    {self.networkx_graph.edges()}")
+        
 
         networkx.draw_networkx(self.networkx_graph, 
             pos = networkx.circular_layout(self.networkx_graph), node_color = self.get_color_map(len(self.networkx_graph)))
         
-        if edge_labels:
+        if edge_labels and not suppress_text_output:
             networkx.draw_networkx_edge_labels(self.networkx_graph, edge_labels = edge_labels, pos = networkx.circular_layout(self.networkx_graph))
 
         if picture_name != "":
