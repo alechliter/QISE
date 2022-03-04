b9c54d56d6cf46af0d420e3cbc2426d88827c128
alechliter <lechliter.2@osu.edu>
2022-03-04 09:56:26 -0500

    Removed debugging print statements. Updated get_all_possible_labels with bug fix from early.

--- src/LabelSetting/Models/LabelAlgorithm.py
+++ src/LabelSetting/Models/LabelAlgorithm.py
@@ -201,6 +201,7 @@ class LabelAlgorithm:
             #   meaning: given node i, select node index k from the list of untreated incoming nodes to i such 
             #            that the weight of the edge (k, i) is the smallest among incoming edges to i
             current_node: NodeLabel = self._rec_next_node(self.source_node)
+            print(f"Current Node: {current_node.node_index}")
             if current_node is not None:
                 i = current_node.node_index
                 k_in: int = None
@@ -210,15 +211,22 @@ class LabelAlgorithm:
                 if current_node.node_index != self.source_node:
                     # find the incoming node with a label with the lowest weight
                     for j_in in current_node.get_untreated_nodes():
-                        if k_in is not None:
-                            j_label = self.node_labels[j_in].get_lowest_weight_label()[1]
+                        j_label = self.node_labels[j_in].get_lowest_weight_label()[1]
+                        if k_label is not None and j_label is not None:
                             if j_label[0] < k_label[0] or (j_label[0] == k_label[0] and j_label[1] < k_label[1]):
+                                k_in = j_in
                                 k_label = j_label
-                        else:
+                        elif k_label is None:
                             k_in = j_in
-                            k_label = self.node_labels[k_in].get_lowest_weight_label()[1]
-                    W_k_i = k_label[0] + self.graph.wc_edges[k_in, i][0]    # the weight of the path [s, ..., k, i]
-                    C_k_i = k_label[1] + self.graph.wc_edges[k_in, i][1]    # the cost of the path [s, ..., k, i]
+                            k_label = j_label
+                    if k_label is not None:
+                        W_k_i = k_label[0] + self.graph.wc_edges[k_in, i][0]    # the weight of the path [s, ..., k, i]
+                        C_k_i = k_label[1] + self.graph.wc_edges[k_in, i][1]    # the cost of the path [s, ..., k, i]
+                    else:
+                        # Special case: only incoming nodes that don't have any labels are untreated, skip for now.
+                        current_node.treated_nodes.append(k_in)
+                        current_node.needs_visit = False
+                        continue
                 else:
                     # current node is the source node, so no incoming nodes exists (that we care about)
                     k_in = current_node.node_index
@@ -310,7 +318,7 @@ class LabelAlgorithm:
         # case 1: all nodes have been treated from this tree 
         # (found_node is a leaf - a node with no outgoing nodes)
         next_node: NodeLabel | None = None
-        # print(f"{from_node}")
+
         if self.node_labels[from_node].num_untreated_nodes() > 0:
             # case 2: from_node has untreated nodes
             next_node = self.node_labels[from_node]
@@ -329,12 +337,7 @@ class LabelAlgorithm:
                 else:
                     # case 4: all child nodes have treated nodes: call _rec_next_node on each until one is found
                     for child in child_nodes:
-                        # print(f"Parent: {self.node_labels[from_node].node_index}  Child: {child_node.node_index}")
                         next_node = self._rec_next_node(child)
                         if next_node is not None:
                             break
-        # if next_node:
-        #     print(next_node.node_index)
-        # else:
-        #     print("No node found")
         return next_node
